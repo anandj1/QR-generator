37b4d2016f0d48673d8cb226aa81fae44a4e5deb
anandj1 <anandj12215@gmail.com>
2024-07-14 00:24:44 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily : {
+      'nunito':[ "Nunito", 'sans-serif']
+    },
+    extend: {},
+  },
+  
+  plugins: [],
+}
+
