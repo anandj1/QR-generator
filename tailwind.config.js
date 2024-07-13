421620f77f334affc0799c730a53db376cf41340
anandj1 <anandj12215@gmail.com>
2024-07-14 00:59:15 +0530

    Second commmit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,14 @@ module.exports = {
     fontFamily : {
       'nunito':[ "Nunito", 'sans-serif']
     },
+    screens: {
+      'xs':{max:'640px'},
+      'sl':{max:'700px'},
+    },
     extend: {},
-  },
+  
   
   plugins: [],
+}
 }
 
