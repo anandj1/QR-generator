31a0e75b06cdc299e08df4ba4c563d908ad6aba8
anandj1 <anandj12215@gmail.com>
2024-07-14 02:13:14 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: 'index.html',
-      external: ['main.css'],
-      onwarn: (warning, warn) => {
-        if (warning.code === 'UNRESOLVED_IMPORT' && warning.source === 'main.css') {
-          return;
-        }
-        warn(warning);
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
       },
+      external: ['qrcode'],
+      output: {
+        globals: {
+          qrcode: 'QRCode',
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '/@': path.resolve(__dirname, 'src'),
     },
   },
 });
